7df3eb46ca1e1e853583a3245f4414c1ad950bcb
Lior Hassin <liorhassin3@gmail.com>
2024-06-30 20:07:52 +0300

    db connection, game model initialization/schema, index.ts changes

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express, { Express } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+import connectDB from "./config/dataBaseConnection";
+
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+const app: Express = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+// Example : app.use("/cards", creditCardRouter);
+
+app.listen(port, () => {
+  try {
+    connectDB();
+  } catch (error) {
+    console.log("Server - connection failed to MongoDB");
+  } finally {
+    console.log(`Server is running at http://localhost:${port}`);
+  }
+});
---
+++ src/models/Game.Schema.ts
@@ -0,0 +1,73 @@
+import mongoose, { Types } from "mongoose";
+
+const supportedPlatforms = [
+  "Steam",
+  "Epic Games",
+  "Origin",
+  "GOG",
+  "Uplay",
+  "Battle.net",
+  "Microsoft Store",
+  "PlayStation Store",
+  "Xbox Store",
+  "Nintendo eShop",
+  "Google Play Store",
+  "Apple App Store",
+];
+
+export type GameType = {
+  name: string;
+  price: number;
+  description: string;
+  developerId: mongoose.Schema.Types.ObjectId;
+  platformLinks: { platform: string; url: string }[];
+  releaseDate: Date;
+};
+
+const GameSchema = new mongoose.Schema<GameType>({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 2,
+    maxlength: 40,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 300,
+  },
+  description: {
+    type: String,
+    required: true,
+    minlength: 10,
+    maxlength: 200,
+    trim: true,
+  },
+  developerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  platformLinks: [
+    {
+      platform: {
+        type: String,
+        required: true,
+        enum: supportedPlatforms,
+        trim: true,
+      },
+      url: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+    },
+  ],
+  releaseDate: {
+    type: Date,
+    required: true,
+  },
+});
+
+export default mongoose.model<GameType>("game", GameSchema, "games");
